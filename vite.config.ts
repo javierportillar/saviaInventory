6242a5c51f8a659a93704e4107a2702078aed4ea
Javier Portilla Rosero <119441100+javierportillar@users.noreply.github.com>
2025-09-21 21:31:57 -0500

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/saviaInventory/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
